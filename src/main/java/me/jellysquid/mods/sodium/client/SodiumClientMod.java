d5323373cc4b48bcbc6262f87e1760f177aab8b5
Noryea <88097529+Noryea@users.noreply.github.com>
2021-11-20 14:29:49 +0800

    Update to 1.18-pre5

---
+++ src/main/java/me/jellysquid/mods/sodium/client/SodiumClientMod.java
@@ -0,0 +1,63 @@
+package me.jellysquid.mods.sodium.client;
+
+import me.jellysquid.mods.sodium.client.gui.SodiumGameOptions;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.ModContainer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+
+public class SodiumClientMod implements ClientModInitializer {
+    private static SodiumGameOptions CONFIG;
+    private static Logger LOGGER;
+
+    private static String MOD_VERSION;
+
+    @Override
+    public void onInitializeClient() {
+        ModContainer mod = FabricLoader.getInstance()
+                .getModContainer("sodium")
+                .orElseThrow(NullPointerException::new);
+
+        MOD_VERSION = mod.getMetadata()
+                .getVersion()
+                .getFriendlyString();
+
+        LOGGER = LogManager.getLogger("Sodium");
+        CONFIG = loadConfig();
+    }
+
+    public static SodiumGameOptions options() {
+        if (CONFIG == null) {
+            throw new IllegalStateException("Config not yet available");
+        }
+
+        return CONFIG;
+    }
+
+    public static Logger logger() {
+        if (LOGGER == null) {
+            LOGGER = LogManager.getLogger("Sodium");
+        }
+
+        return LOGGER;
+    }
+
+    private static SodiumGameOptions loadConfig() {
+        return SodiumGameOptions.load(FabricLoader.getInstance().getConfigDir().resolve("sodium-options.json"));
+    }
+
+    public static String getVersion() {
+        if (MOD_VERSION == null) {
+            throw new NullPointerException("Mod version hasn't been populated yet");
+        }
+
+        return MOD_VERSION;
+    }
+
+    public static boolean isDirectMemoryAccessEnabled() {
+        return options().advanced.allowDirectMemoryAccess;
+    }
+}
